e571fceb4a39c5bb2c8fe554053341f69fb744be
facuscrollini <facuscrollinic@gmail.com>
2026-04-08 21:31:40 +0200

    models and serialized created, doing views and urls

--- backend/apps/blog/views.py
+++ backend/apps/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Post
+from .serializers import PostListSerializer, PostSerializer
 
-# Create your views here.
+class PostListView(ListAPIView):
+    queryset = Post.postobjects.all()
+    serializer_class = PostListSerializer
+
+class PostDetailView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer = PostSerializer
+    lookup_field = 'slug'
